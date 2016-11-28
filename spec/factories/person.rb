9ebaa9d27510f64b8e88b5832a12bc898fdf8f0a
rafael simao <raficher@gmail.com>
2016-11-28 17:56:41 -0200

    Changed person factory(to use trait)
    Changed the Person factory so that it used trait instead of inhearetance

--- spec/factories/person.rb
+++ spec/factories/person.rb
@@ -2,10 +2,10 @@
 FactoryGirl.define do
   factory :person do
     name  "Diretora"
-  end
 
-  factory :full_person, parent: :person do
-    email "test@gmail.com"
-    phone "3562-4588"
+    trait :complete do
+      email "test@gmail.com"
+      phone "3562-4588"
+    end
   end
 end
