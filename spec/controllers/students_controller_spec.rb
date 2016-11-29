8631bbd2f6b350682123b109a4e9ce5eb29d1bb6
rafael simao <raficher@gmail.com>
2016-11-29 17:31:32 -0200

    Created StudentsController
    Created StudentsController with its restful actions

---
+++ spec/controllers/students_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe StudentsController, type: :controller do
+
+  describe "GET #index" do
+  end
+
+  describe "GET #show" do
+  end
+
+  describe "GET #new" do
+  end
+
+  describe "POST #create" do
+  end
+
+  describe "GET #edit" do
+  end
+
+  describe "PATCH #update" do
+  end
+
+  describe "DELETE #destroy" do
+  end
+
+end
