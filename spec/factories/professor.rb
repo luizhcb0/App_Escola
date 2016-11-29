c4d738bd125be3f0928e1b82317adb1a7024f343
rafael simao <raficher@gmail.com>
2016-11-29 10:52:31 -0200

    Added Professor and School factories traits
    Added stubbed traits to both School and Professor.

--- spec/factories/professor.rb
+++ spec/factories/professor.rb
@@ -3,5 +3,10 @@ FactoryGirl.define do
   factory :professor do
     person
     login
+
+    trait :stubed do
+      association :person
+      association :login
+    end
   end
 end
--- spec/factories/schools.rb
+++ spec/factories/schools.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
   factory :school do
     name  "Escola Teste"
     professor
+
+    trait :stubed do
+      association :professor, :stubed
+    end
   end
 end
