4664cfac67045d85320c54388329aa9ebd8b4cba
rafael simao <raficher@gmail.com>
2016-12-01 21:20:38 -0200

    Added professors show view

--- spec/views/professors/show.html.erb_spec.rb
+++ spec/views/professors/show.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "professors/show.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) {
+    assign(:professor, build(:professor, id:2))
+    render
+  }
+
+  it "should show school atributes" do
+    expect(rendered).to match(/Diretora/)     # person name
+    expect(rendered).to match(/Vira Ventos/)  # login username
+  end
 end
