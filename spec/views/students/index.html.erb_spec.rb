e5917e3462989caf40f7735e6821deba8512e639
rafael simao <raficher@gmail.com>
2016-12-01 21:32:39 -0200

    Added student index view

--- spec/views/students/index.html.erb_spec.rb
+++ spec/views/students/index.html.erb_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "students/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) { assign(:students, [
+    Student.new(id: 1, person: Person.new(name: "Student1")),
+    Student.new(id: 2, person: Person.new(name: "Student2")),
+    Student.new(id: 3, person: Person.new(name: "Student3")) ])
+    render
+  }
+
+  it "should display all assigned Students" do
+    expect(rendered).to match(/Student1/)
+    expect(rendered).to match(/Student2/)
+    expect(rendered).to match(/Student3/)
+  end
 end
