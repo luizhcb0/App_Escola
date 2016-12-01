ded524eea354698523b7c7b0f6716fb6e6a46deb
rafael simao <raficher@gmail.com>
2016-12-01 21:27:17 -0200

    Added professors new and edit views
    Also create professors form.

--- spec/views/professors/edit.html.erb_spec.rb
+++ spec/views/professors/edit.html.erb_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "professors/edit.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) {
+    assign(:professor, build(:professor, id: 2) )
+    render
+  }
+
+  it "should render form" do
+    expect(view).to render_template(:partial => "_form")
+  end
+
+  it "should render person_fields partial" do
+    expect(view).to render_template(:partial => "_person_fields")
+  end
+
+  it "should render login_fields partial" do
+    expect(view).to render_template(:partial => "_login_fields")
+  end
 end
