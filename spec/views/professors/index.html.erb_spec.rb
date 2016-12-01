3b25ba32ef8de6d2c70d902ea21057290d46c327
rafael simao <raficher@gmail.com>
2016-12-01 21:05:39 -0200

    Added professor index view

--- spec/views/professors/index.html.erb_spec.rb
+++ spec/views/professors/index.html.erb_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "professors/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) { assign(:professors, [
+    Professor.new(id: 1, person: Person.new(name: "Professor1")),
+    Professor.new(id: 2, person: Person.new(name: "Professor2")),
+    Professor.new(id: 3, person: Person.new(name: "Professor3")) ])
+    render
+  }
+
+  it "should display all assigned professors" do
+    expect(rendered).to match(/Professor1/)
+    expect(rendered).to match(/Professor2/)
+    expect(rendered).to match(/Professor3/)
+  end
 end
