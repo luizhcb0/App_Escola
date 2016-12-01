40c6ca0200ced09db92748f70920ade1586ffc1b
rafael simao <raficher@gmail.com>
2016-12-01 21:37:39 -0200

    Added students show view

--- spec/views/students/show.html.erb_spec.rb
+++ spec/views/students/show.html.erb_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "students/show.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) {
+    assign(:student, build(:student, id:2))
+    render
+  }
+
+  it "should show school atributes" do
+    expect(rendered).to match(/Diretora/)     # person name
+    expect(rendered).to match(/Pai/)          # father_name
+    expect(rendered).to match(/Mae/)          # mother_name
+  end
 end
