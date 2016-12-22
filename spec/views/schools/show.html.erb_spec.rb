3fa583b43581685a8afa92a14bea8f394e5f2fe4
rafael simao <raficher@gmail.com>
2016-12-21 23:39:20 -0200

    Created: user_show partial
    Used to show user informations on Schools, Students and Professors.

--- spec/views/schools/show.html.erb_spec.rb
+++ spec/views/schools/show.html.erb_spec.rb
@@ -8,7 +8,13 @@ RSpec.describe "schools/show.html.erb", type: :view do
 
   it "should show school atributes" do
     expect(rendered).to match(/Escola Teste/) # school name
+  end
+
+  it "should show school principal user attributes" do
     expect(rendered).to match(/User/)         # user name
   end
 
+  it "should render user_show partial" do
+    expect(view).to render_template(:partial => "_user_show")
+  end
 end
--- spec/views/students/show.html.erb_spec.rb
+++ spec/views/students/show.html.erb_spec.rb
@@ -14,4 +14,8 @@ RSpec.describe "students/show.html.erb", type: :view do
   it "should show user atributes" do
     expect(rendered).to match(/User/)     # name
   end
+
+  it "should render user_show partial" do
+    expect(view).to render_template(:partial => "_user_show")
+  end
 end
