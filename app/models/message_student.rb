443bcf727bf7958ffe8bf28f435fe103b3b0ffe0
rafael simao <raficher@gmail.com>
2017-02-21 14:38:47 -0300

    Added view messages to report
    Get all the unread messages(including nil) and display at the warning
    pool.

--- app/models/message_student.rb
+++ app/models/message_student.rb
@@ -1,4 +1,11 @@
 class MessageStudent < ApplicationRecord
   belongs_to :message
   belongs_to :student
+
+  # Callback to set the read proprerty false on Create
+  before_create :set_not_read
+
+  def set_not_read
+    self.read = false
+  end
 end
