b785534ff3f2ed1a6b6700ad1659088ca7191127
rafael simao <raficher@gmail.com>
2017-02-23 17:34:50 -0300

    Added: Report suboptions are grouped toguether
    Creates a join selection of the sub options and order then by activity,
    option and sub options. Then, present them in a grouped manner.

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -23,4 +23,17 @@ module ReportsHelper
     suboptions = Suboption.all
   end
 
+  # Creates a join between the report suboptions and options, activities and
+  # activities_categories, filtring the columns to keep only names and ids.
+  def suboptions_with_joins(report_id)
+    Report.connection.select_all("select suboption_id, suboptions.name as suboption_name,
+    option_id, options.name as option_name, activity_id, activities.name as activity_name,
+    activity_category_id from
+      (select suboption_id from reports_suboptions where report_id=#{report_id}) as rep_subs
+      join suboptions on rep_subs.suboption_id=suboptions.id
+      join options on suboptions.option_id=options.id
+      join activities on options.activity_id=activities.id
+      order by activity_category_id, activity_id, option_id, suboption_id")
+  end
+
 end
