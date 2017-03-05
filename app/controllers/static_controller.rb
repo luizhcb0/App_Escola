42efdc09611c5e5394797674b0530b97938eb22a
rafael simao <raficher@gmail.com>
2017-03-05 18:43:52 -0300

    Redirect student to report page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,11 @@
 class StaticController < ApplicationController
+  include SessionsHelper
+
   def index
+    if !current_user.nil? &&
+      (current_user.role.name == 'student' || current_user.role.name == "student_adjacent")
+      redirect_to student_report_path(current_user.students[0].id, Date.today)
+    end
   end
+
 end
