de84e4d10fedd6c52c3ff21ce465758c3c5fbaea
rafael simao <raficher@gmail.com>
2017-03-10 23:23:32 -0300

    Added: report_notes to report/new
    Now it is possible to create a note on an activity when you create a
    new report.

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,7 +3,8 @@ class Report < ApplicationRecord
   has_and_belongs_to_many :suboptions
   has_many :report_notes
 
-  accepts_nested_attributes_for :report_notes
+  accepts_nested_attributes_for :report_notes, allow_destroy: true,
+    reject_if: ->(attrs) { attrs['text'].blank? }
 
   # Callback to set the read proprerty false on Create
   before_create :set_date
