64e0cf25be507f4c989643aefc38651bea451a94
rafael simao <raficher@gmail.com>
2017-04-03 14:18:47 -0300

    Fixed: message tests
    Fixed message to pass in the view tests

--- spec/views/reports/new.html.erb_spec.rb
+++ spec/views/reports/new.html.erb_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "reports/new.html.erb", type: :view do
   before(:each) {
+    view.extend SessionsHelper
     assign(:report, Report.new )
     assign(:categories, ActivityCategory.all )
+    professor = create(:professor)
+    assign(:current_user, professor.user)
     render
   }
 
