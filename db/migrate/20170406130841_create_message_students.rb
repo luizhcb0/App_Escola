56e82f907a73764656c1c2cb4e27f0fab413ef47
rafael simao <raficher@gmail.com>
2017-04-06 10:18:22 -0300

    Little migration fix

--- db/migrate/20170406130841_create_message_students.rb
+++ db/migrate/20170406130841_create_message_students.rb
@@ -1,8 +1,8 @@
 class CreateMessageStudents < ActiveRecord::Migration[5.0]
   def change
     create_table :message_students do |t|
-      t.belongs_to :messages, null: false, index: true
-      t.belongs_to :students, null: false, index: true
+      t.belongs_to :message, null: false, index: true
+      t.belongs_to :student, null: false, index: true
       t.boolean :read
     end
     add_foreign_key :message_students, :messages, on_delete: :cascade
