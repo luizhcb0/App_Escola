00f7e7da0d9fa401d105cac9260ea73563cebf30
rafael simao <raficher@gmail.com>
2017-05-01 10:48:10 -0300

    Added: attributes to the db
    Added title and description to clip

--- db/migrate/20170427144740_create_clips.rb
+++ db/migrate/20170427144740_create_clips.rb
@@ -1,6 +1,8 @@
 class CreateClips < ActiveRecord::Migration[5.0]
   def change
     create_table :clips do |t|
+      t.string :title
+      t.text :description
       t.attachment :media
       t.timestamps
     end
