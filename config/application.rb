52688c25453c46a57e3b82065ff0ddaade5cae2b
luizhcb0 <luizhcb0@gmail.com>
2017-05-01 03:20:49 -0300

    Set Timezone

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module AppAgenda
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += %W(#{config.root}/lib)
+    config.timezone = "Brasilia"
   end
 end
